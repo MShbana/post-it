38eacf907799ab404354bbfd385a95d516aec0b5
MShbana <M.Shabana@protonmail.ch>
2019-07-23 18:04:10 +0200

    Defined the MEDIA_URL and MEDIA_ROOT for the static uploaded files, and decreased the constraints for creating UserProfile

--- accounts/models.py
+++ accounts/models.py
@@ -16,11 +16,11 @@ class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
-    image = models.ImageField(default='default.jpg', upload_to='profile_pics', blank=True)
-    city = models.CharField(max_length=20)
-    country = fields.CountryField()
-    linkedin = models.URLField()
-    gender =  models.CharField(max_length=1, choices=GENDER_CHOICES)
+    image = models.ImageField(default='default.png', upload_to='profile_pics', blank=True)
+    city = models.CharField(max_length=20, blank=True)
+    country = fields.CountryField(blank=True)
+    linkedin = models.URLField(blank=True)
+    gender =  models.CharField(max_length=1, choices=GENDER_CHOICES, blank=True)
 
     def __str__(self):
         return f'{self.user} Profile'
@@ -30,6 +30,7 @@ class UserProfile(models.Model):
 
     class Meta:
         verbose_name_plural = 'Users Profiles'
+        ordering = ('-created', )
 
     @receiver(post_save, sender=User)
     def create_userprofile(sender, instance, created, **kwargs):

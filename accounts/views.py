2bacac20b940ff505636d5fb6839dea569df2774
MShbana <M.Shabana@protonmail.ch>
2019-07-23 18:24:56 +0200

    Created the base for the User's Profile Page and provided links for it, from and to the homepage

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def view_profile(request):
+    args = {'user': request.user}
+    return render(request, 'accounts/view_profile.html', args)
--- home/urls.py
+++ home/urls.py
@@ -4,7 +4,6 @@ from django.urls import path
 
 app_name = 'home'
 
-
 urlpatterns = [
-    path('', views.HomeView.as_view(), name='home_view'),
+    path('', views.HomeView.as_view(), name='home'),
 ]

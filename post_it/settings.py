193ca171b68c3b0006cda703c74b95c767336844
MShbana <M.Shabana@protonmail.ch>
2019-07-24 09:31:52 +0200

    Added login requirement for accessing account views

--- post_it/settings.py
+++ post_it/settings.py
@@ -125,3 +125,7 @@ STATIC_URL = '/static/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 MEDIA_URL = '/media/'
 
+
+# Authentication & Authorization
+# LOGIN_URL = 'login'
+# LOGIN_REDIRECT_URL = 'home'

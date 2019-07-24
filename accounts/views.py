7e47a42a5048bd4f56eb9df864dc03a5407190da
MShbana <M.Shabana@protonmail.ch>
2019-07-24 11:16:49 +0200

    Fixed navbar items depending on whether the user is authenticated or not

--- accounts/views.py
+++ accounts/views.py
@@ -2,11 +2,13 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from django.shortcuts import render
 
+
 @login_required
 def view_account(request, username):
     user = User.objects.get(username=username)
     args = {'user': user}
     return render(request, 'accounts/view_account.html', args)
+
 
 @login_required
 def view_account_info(request, username):
--- home/urls.py
+++ home/urls.py
@@ -1,8 +1,9 @@
 from . import views
 from django.urls import path
 
+
 app_name = 'home'
 
 urlpatterns = [
-    path('',views.HomeView.as_view(), name='home'),
+    path('', views.HomeView.as_view(), name='home'),
 ]

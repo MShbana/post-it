900e2e088f6dc121737f50dd4aa889f28003b856
MShbana <M.Shabana@protonmail.ch>
2019-07-25 16:46:27 +0200

    Added First and last_name fields to the UserRegisteration form and registered the admin site with the new UserCreation form and fields

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,11 +1,27 @@
+from .forms import UserRegisterationForm
 from .models import UserProfile
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import User
 
 
 @admin.register(UserProfile)
 class UserProfileAdmin(admin.ModelAdmin):
+    
     list_display = ('user', 'slug', 'city', 'country', 'gender', 'linkedin')
     list_filter = ('created', 'gender', 'city', 'country')
     ordering = ('-created', 'user__username')
     search_fields = ('user__username', 'linkedin')
     prepopulated_fields = {'slug': ('user', )}
+
+
+class CustomUserAdmin(UserAdmin):
+    
+    # form = UserUpdateForm
+    add_form = UserRegisterationForm
+    add_fieldsets = (
+        (None, {'fields': ('username', 'first_name', 'last_name', 'email', 'password1', 'password2'),}),)
+
+
+admin.site.unregister(User)
+admin.site.register(User, CustomUserAdmin)
--- accounts/forms.py
+++ accounts/forms.py
@@ -3,19 +3,20 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
 
-class EmailRequiredMixin(object):
-    
+class ExtraFieldsRequiredMixin(object):
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.fields['email'].required = True
+        self.fields['first_name'].required = True
+        self.fields['last_name'].required = True
 
-
-class UserRegisterationForm(EmailRequiredMixin, UserCreationForm):
+class UserRegisterationForm(ExtraFieldsRequiredMixin, UserCreationForm):
 
     class Meta:
         model = User
-        fields = UserCreationForm.Meta.fields + ('email', )
-    
+        fields = UserCreationForm.Meta.fields + ('first_name', 'last_name', 'email')
+
     def clean_email(self):
         email = self.cleaned_data['email']
         if User.objects.filter(email=email).exists():

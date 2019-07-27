adfae15d03dc454a98c168ba48336d7b0ea67347
MShbana <M.Shabana@protonmail.ch>
2019-07-27 15:44:58 +0200

    Implemented the home's GET funcionality

--- posts/views.py
+++ posts/views.py
@@ -1,6 +1,13 @@
+from .models import Post
 from django.shortcuts import render
 from django.views.generic import TemplateView
 
 
 class Home(TemplateView):
+    
     template_name = 'posts/home.html'
+
+    def get(self, request):
+        posts = Post.objects.all()
+        args = {'posts': posts}
+        return render(request, self.template_name, args)

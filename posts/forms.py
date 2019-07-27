21b28f04fa169ad378db430c95eb67cbe4459bd3
MShbana <M.Shabana@protonmail.ch>
2019-07-27 16:37:54 +0200

    Added the POST funcionality for the home's Post form

--- posts/forms.py
+++ posts/forms.py
@@ -1,5 +1,4 @@
 from .models import Post
-from crispy_forms.helper import FormHelper
 from django import forms
 
 
@@ -7,11 +6,10 @@ class PostCreationForm(forms.ModelForm):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.helper = FormHelper()
-        self.helper.form_show_labels = False
+        self.fields['title'].label = False
+        self.fields['body'].label = False
         self.fields['title'].widget.attrs['placeholder'] = 'Your Post Title'
         self.fields['body'].widget.attrs['placeholder'] = 'Your Post'
-
 
     class Meta:
         model = Post
--- posts/views.py
+++ posts/views.py
@@ -1,6 +1,6 @@
 from .forms import PostCreationForm
 from .models import Post
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic import TemplateView
 
 
@@ -12,4 +12,15 @@ class Home(TemplateView):
         form = PostCreationForm()
         posts = Post.objects.all()
         args = {'posts': posts, 'form': form}
+        return render(request, self.template_name, args)
+
+    def post(self, request):
+        form = PostCreationForm(request.POST)
+        if form.is_valid():
+            saved_post = form.save(commit=False)
+            saved_post.user = request.user
+            saved_post.save()
+            return redirect('posts:home')
+
+        args = {'form': form}
         return render(request, self.template_name, args)

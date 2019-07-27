09573013e47eb42eb6e7e867ef1a1baec8c3e7bd
MShbana <M.Shabana@protonmail.ch>
2019-07-27 11:57:45 +0200

    Modified the Profile save method to resize the uploaded profile pics

--- accounts/models.py
+++ accounts/models.py
@@ -2,6 +2,7 @@ from django_countries import fields
 from django.contrib.auth.models import User
 from django.db import models
 from django.utils.text import slugify
+from PIL import Image
 
 
 class Profile(models.Model):
@@ -44,3 +45,10 @@ class Profile(models.Model):
     def save(self, *args, **kwargs):
         self.slug = slugify(self.user)
         super().save(*args, **kwargs)
+
+        img = Image.open(self.image.path)
+
+        if img.height > 300 or img.width > 300:
+            output_size = (300, 300)
+            img.thumbnail(output_size)
+            img.save(self.image.path)

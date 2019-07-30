7d231ad0e96d359cb702271884f4ecd41d5ce1a3
MShbana <M.Shabana@protonmail.ch>
2019-07-30 16:10:17 +0200

    Set Debug to False

--- post_it/settings.py
+++ post_it/settings.py
@@ -26,7 +26,9 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = (os.environ.get('DEBUG_VALUE') == 'True')
+# DEBUG = (os.environ.get('DEBUG_VALUE') == 'True')
+DEBUG = os.environ.get("DEBUG", False)
+
 
 ALLOWED_HOSTS = ['post-itapp.herokuapp.com']
 
